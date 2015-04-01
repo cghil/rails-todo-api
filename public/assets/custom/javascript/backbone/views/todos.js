629ef888d152c9d2a9efce8f79aba869efed9fb1
cghil <cghilarducci@gmail.com>
2015-04-01 11:40:14 -0700

    events hash now works, comment has been added

--- public/assets/custom/javascript/backbone/views/todos.js
+++ public/assets/custom/javascript/backbone/views/todos.js
@@ -1,14 +1,30 @@
 var app = app || {};
 
-var TodoView = Backbone.View.extend({
+app.TodoView = Backbone.View.extend({
 
 	el: '.empty',
+
+	events: {
+		'dblclick .edit-description': 'edit',
+		'click .edit-item': 'log'
+	},
 
 	template: _.template( $('#item-template').html() ),
 
 	render: function(){
-		this.$el.after( this.template( this.model.attributes) );
+		this.$el.html( this.template( this.model.attributes) );
 		debugger;
+	},
+
+	// if you use an .after() instead of .html el has no children thus, el is unable to connect events
+	// using the events hash
+
+	edit: function(){
+		console.log('hope this works');
+	},
+
+	log: function(){
+		console.log('yup this is a click')
 	}
 
 });
