12aa107b024681d7a800589a4b76dd1268342acd
cghil <cghilarducci@gmail.com>
2015-04-13 16:47:47 -0700

    templates for signup and signin, sessionStorage checker

--- public/assets/custom/javascript/backbone/app.js
+++ public/assets/custom/javascript/backbone/app.js
@@ -1,7 +1,9 @@
 var app = app || {};
 $(function(){
-	if (sessionStorage.length === 0){
+	if (sessionStorage.length === 0 && sessionStorage.getItem('user_id') === null){
 		debugger
+		$('div#maingrid').hide();
+		// render the signin and signup views
 	}
 	app.View = new app.AppView();
 })
